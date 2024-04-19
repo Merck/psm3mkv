20b8f4b24b9190884b42c894314aa8bd5848ee20
Nan Xiao <me@nanx.me>
2024-04-18 21:13:23 -0400

    Restore file path and add dev script to `.Rbuildignore`

--- inst/logo/logo.R
+++ inst/logo/logo.R
@@ -7,6 +7,6 @@ hexSticker::sticker(
   s_x = 0.98, s_y = 0.95, s_width = 1.075, s_height = 1.075,
   package = "", p_x = 1, p_y = 1, p_size = 8, h_size = 1.2, p_family = "pf",
   p_color = "#7B1F58", h_fill = "#EFDBE5", h_color = "#7B1F58",
-  dpi = 640, filename = file.path(tempdir(), "logo.png"),
+  dpi = 640, filename = "man/figures/logo.png",
   white_around_sticker = TRUE
 )
