56dcfd93788c5f18b98edd63c281611072ec9c36
Dominic Muston <dom.muston@gmail.com>
2024-04-21 18:00:52 -0400

    Revise logo

--- inst/logo/logo.R
+++ inst/logo/logo.R
@@ -4,9 +4,11 @@
 
 hexSticker::sticker(
   subplot = "inst/logo/greenlogo.png",
-  s_x = 0.98, s_y = 0.95, s_width = 1.075, s_height = 1.075,
+  s_x = 1, s_y = 1, s_width = 0.8, s_height = 0.8,
   package = "", p_x = 1, p_y = 1, p_size = 8, h_size = 1.2, p_family = "pf",
   p_color = "#00857C", h_fill = "#FFFFFF", h_color = "#00857C",
   dpi = 640, filename = "man/figures/logo.png",
   white_around_sticker = TRUE
 )
+
+
