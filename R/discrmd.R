258f4caa48a096f507c425abe1269064444773f9
Dominic Muston <dominic.muston@merck.com>
2024-06-03 14:32:19 -0400

    Revise drmd_stm_cr

--- R/discrmd.R
+++ R/discrmd.R
@@ -277,20 +277,24 @@ drmd_stm_cf <- function(dpam, Ty=10, discrate=0, lifetable=NA, timestep=1) {
     tmid = tzero + timestep/2,
     u_pf = prob_pf_stm(tzero, dpam),
     u_pd = prob_pd_stm_cf(tzero, dpam),
-    # Calculate hazards
+    # Calculate PPD hazard and probability
     h_ppd = calc_haz(tmid, survobj=dpam$ppd),
-    h_pps = calc_haz(tmid, survobj=dpam$pps_cf),
-    # Unconstrained mortality hazards
     q_ppd = 1-exp(-h_ppd),
-    q_pps = 1-exp(-h_pps),
     # Derive the constrained life table
     clx = calc_ltsurv(convert_wks2yrs(ds$tzero), lifetable),
     # Derive the background mortality for this timepoint
     cqx = 1 - dplyr::lead(clx)/clx,
-    # Derive the TTP probability (balancing item)
+    # Derive the TTP probability (balancing item for PFS)
     q_pfs = 1 - dplyr::lead(u_pf)/u_pf,
     q_ttp = q_pfs-q_ppd,
     cqpfs = q_ttp + pmax(q_ppd, cqx),
+    # Derive the PPS mortality probability
+    d_pfpd = u_pf + u_pd - dplyr::lead(u_pf) - dplyr::lead(u_pd),
+    d_pps = d_pfpd - d_pf,
+    q_pps = dplyr::if_else(u_pd==0, 0, d_pps / u_pd),
+    # Constrained probabilities
+    cqpfs = q_ttp + pmax(q_ppd, cqx),
+    cqpps = pmax(q_pps, cqx),
     # Initial constrained PF and PD
     c_pf = u_pf,
     c_pd = u_pd
@@ -338,11 +342,11 @@ drmd_stm_cf <- function(dpam, Ty=10, discrate=0, lifetable=NA, timestep=1) {
 #   pps_cf = find_bestfit_spl(fits$pps_cf, "aic")$fit,
 #   pps_cr = find_bestfit_spl(fits$pps_cr, "aic")$fit
 # )
-# drmd_stm_cr(dpam=params)
+# drmd1_stm_cr(dpam=params)
 # # Add a lifetable constraint
 # ltable <- tibble::tibble(lttime=0:20, lx=1-lttime*0.05)
-# drmd_stm_cr(dpam=params, lifetable=ltable)
-drmd_stm_cr <- function(dpam, Ty=10, discrate=0, lifetable=NA, timestep=1) {
+# drmd1_stm_cr(dpam=params, lifetable=ltable)
+drmd1_stm_cr <- function(dpam, Ty=10, discrate=0, lifetable=NA, timestep=1) {
   # Declare local variables
   Tw <- tvec <- sppd <- sttp <- sos <- NULL
   adjsppd <- adjos <- vn <- pf <- os <- NULL
@@ -385,4 +389,85 @@ drmd_stm_cr <- function(dpam, Ty=10, discrate=0, lifetable=NA, timestep=1) {
   return(list(pf=pf, pd=pd, os=pf+pd))
 }
 
-
+#' Discretized Restricted Mean Duration calculation for State Transition Model Clock Reset structure
+#' Calculate restricted mean duration (RMD) in PF, PD and OS states under a State Transition Model Clock Reset structure.
+#' @inherit drmd_psm params return
+#' @seealso [drmd_stm_cf()] [drmd_psm()]
+#' @noRd
+# Examples
+# # Create dataset and fit survival models (splines)
+# bosonc <- create_dummydata("flexbosms")
+# fits <- fit_ends_mods_spl(bosonc)
+# # Pick out best distribution according to min AIC
+# params <- list(
+#   ppd = find_bestfit_spl(fits$ppd, "aic")$fit,
+#   ttp = find_bestfit_spl(fits$ttp, "aic")$fit,
+#   pfs = find_bestfit_spl(fits$pfs, "aic")$fit,
+#   os = find_bestfit_spl(fits$os, "aic")$fit,
+#   pps_cf = find_bestfit_spl(fits$pps_cf, "aic")$fit,
+#   pps_cr = find_bestfit_spl(fits$pps_cr, "aic")$fit
+# )
+# drmd_stm_cr(dpam=params)
+# # Add a lifetable constraint
+# ltable <- tibble::tibble(lttime=0:20, lx=1-lttime*0.05)
+# drmd_stm_cr(dpam=params, lifetable=ltable)
+drmd_stm_cr <- function(dpam, Ty=10, discrate=0, lifetable=NA, timestep=1) {
+  # Declare local variables
+  Tw <- tvec <- sppd <- sttp <- sos <- NULL
+  adjsppd <- adjos <- vn <- pf <- os <- NULL
+  # Time horizon in weeks (ceiling)
+  Tw <- ceiling(convert_yrs2wks(Ty)/timestep)
+  # Create dataset, starting with time vector, with half-cycle addition, and unconstrained probs
+  ds <- tibble::tibble(
+    tzero = timestep*(0:Tw),
+    tmid = tzero + timestep/2,
+    u_pf = prob_pf_stm(tzero, dpam),
+    u_pd = prob_pd_stm_cf(tzero, dpam)
+  )
+  # Keep calculating membership probabilities
+  ds <- ds |>
+    dplyr::mutate(
+    # Calculate PPD hazard and probability
+    h_ppd = calc_haz(tmid, survobj=dpam$ppd),
+    q_ppd = 1-exp(-h_ppd),
+    # Derive the constrained life table
+    clx = calc_ltsurv(convert_wks2yrs(ds$tzero), lifetable),
+    cqx = 1 - dplyr::lead(clx)/clx,
+    # Derive the TTP probability (balancing item for PF)
+    q_pfs = 1 - dplyr::lead(u_pf)/u_pf,
+    q_ttp = q_pfs-q_ppd,
+    d_pf = u_pf * q_ppd,
+    # Derive the PPS mortality probability
+    d_pfpd = u_pf + u_pd - dplyr::lead(u_pf) - dplyr::lead(u_pd),
+    d_pps = d_pfpd - d_pf,
+    q_pps = dplyr::if_else(u_pd==0, 0, d_pps / u_pd),
+    # Constrained probabilities
+    cqpfs = q_ttp + pmax(q_ppd, cqx),
+    cqpps = pmax(q_pps, cqx),
+    # Initial constrained PF and PD
+    c_pf = u_pf,
+    c_pd = u_pd
+  )
+  # Derive the constrained PF and PD memberships
+  for (t in 2:(Tw)) {
+    ds$c_pf[t] = ds$c_pf[t-1] * (1-ds$cqpfs[t-1])
+    ds$c_pd[t] = ds$c_pf[t-1] * ds$q_ttp[t-1] + ds$c_pd[t-1] * (1 - ds$cqpps[t-1])
+  }
+  # The final membership probabilities are zero
+  ds$c_pf[Tw+1] <- ds$c_pd[Tw+1] <- 0
+  # Final calculations on the dataset
+  ds <- ds |>
+    dplyr::mutate(
+      # Discount factor
+      vn = (1+discrate)^(-convert_wks2yrs(tmid)),
+      # RMD components in each timestep
+      rmd_pf = (c_pf + dplyr::lead(c_pf))/2 * vn * timestep,
+      rmd_pd = (c_pd + dplyr::lead(c_pd))/2 * vn * timestep
+    )
+  ds$rmd_pf[Tw+1] <- ds$rmd_pd[Tw+1] <- 0
+  # Calculate RMDs
+  pf <- sum(ds$rmd_pf)
+  pd <- sum(ds$rmd_pd)
+  # Return values
+  return(list(pf=pf, pd=pd, os=pf+pd, calc=ds))
+}
